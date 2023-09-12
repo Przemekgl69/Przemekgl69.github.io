b85d3131d5eda797c1a99a0d7a84acd91712e190
Przemek Glaza <przemek-gl@wp.pl>
2023-09-12 05:18:08 +0200

    adding a heading: experience, skills,

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const img = document.querySelector('img');
-const zoomInButton = document.getElementById('zoom-in-button');
-
-zoomInButton.addEventListener('click', () => {
-  img.style.transform = 'scale(1.2)';
-});
